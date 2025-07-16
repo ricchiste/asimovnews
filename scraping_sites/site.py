326400aaa06a6302f6c0b76de10f1ea23f8f1a26
Ricardo <rjmchiste@gmail.com>
2025-07-16 16:17:01 -0300

    Atualização segundo dia de asimov news, menu de opções

--- scraping_sites/site.py
+++ scraping_sites/site.py
@@ -55,34 +55,8 @@ class Site:
 
             self.news = news_dict_veja
             news_dict_veja
-        if self.opcao.lower() == 'cnn':
-            url = 'https://www.cnnbrasil.com.br/'
-            browsers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \(KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36"}
-            page = requests.get(url, headers = browsers)
-
-            resposta = page.text
-            soup = BeautifulSoup(resposta, 'html.parser')
-            noticias = soup.find_all('a')
-
-            target_class_1 = 'headline__primary_title'
-            target_class_2 = 'home__title'
-            target_class_3 = 'articles__title'
-            target_class_4 = 'webstories_title'
-            news_dict_cnn = {}
-
-            for i in range(len(noticias)):
-                if (noticias[i].h2 != None) and (noticias[i].h2.get('class') != None) and (target_class_1 in noticias[i].h2.get('class')):
-                    news_dict_cnn[noticias[i].h2.text] = noticias[i].get('href')
-                if noticias[i].h3 != None and (noticias[i].h3.get('class') != None):
-                    if target_class_2 in noticias[i].h3.get('class'):
-                        news_dict_cnn[noticias[i].h3.text] = noticias[i].get('href')
-                    if target_class_3 in noticias[i].h3.get('class'):
-                        news_dict_cnn[noticias[i].h3.text] = noticias[i].get('href')
-                    if target_class_4 in noticias[i].h3.get('class'):
-                        news_dict_cnn[noticias[i].h3.text] = noticias[i].get('href')
-
-            self.news = news_dict_cnn
-            news_dict_cnn
+        
 
 
-self = Site('veja')
+self = Site('globo')
+self.update_news()
